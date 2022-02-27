1f3aefebb162d19b871271f5335b72ed3bf4506f
yavuzselim <yavuzselim.kurnaz@beyn.com.tr>
2022-02-27 20:49:36 +0300

    Level 3 workout results moved to separate folder

--- 02_Day_Data_types/level-2.js
+++ 02_Day_Data_types/level-2.js
@@ -74,33 +74,3 @@ console.log(
   string.substring(0, firstBecauseStart),
   string.substring(lastBeacuseAddedBeacuseLength, stringLength)
 );
-
-console.log('=== Level 3 ====');
-// 1- 'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
-const loveString =
-  'Love is the best thing in this world. Some found their love and some are still looking for their love.';
-console.log('1- ', loveString.match(/(love)/gi).length); // g-means to search in the whole text, i - case insensitive
-
-// 2- Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
-const beacuseString =
-  'You cannot end a sentence with because because because is a conjunction';
-console.log('2- ', beacuseString.match(/(because)/gi).length); // g-means to search in the whole text, i - case insensitive
-
-// 3- Clean the following text and find the most frequent word (hint, use replace and regular expressions).
-const dirtySentence =
-  '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
-const regex = /[@#&;!\$%]/g;
-const clearSentences = dirtySentence.replace(regex, '');
-
-console.log('3- ', clearSentences);
-
-// 4- Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
-const earnText =
-  'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.';
-
-const regexForEarnText = /\d+/g;
-const totalEarn = earnText
-  .match(regexForEarnText)
-  .reduce((acc, cur) => Number(acc) + Number(cur));
-
-console.log('4- ', totalEarn);
